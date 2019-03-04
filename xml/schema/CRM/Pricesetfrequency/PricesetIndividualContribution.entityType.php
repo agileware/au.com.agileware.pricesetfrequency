3bcbf922dddb2ea1f2ad718b97549bd6195900d0
Francis Whittle <francis@agileware.com.au>
2019-03-04 13:01:58 +1100

    Fixup style & whitespace.

--- xml/schema/CRM/Pricesetfrequency/PricesetIndividualContribution.entityType.php
+++ xml/schema/CRM/Pricesetfrequency/PricesetIndividualContribution.entityType.php
@@ -1,9 +1,8 @@
 <?php
 // This file declares a new entity type. For more details, see "hook_civicrm_entityTypes" at:
 // http://wiki.civicrm.org/confluence/display/CRMDOC/Hook+Reference
-return array (
-  0 => 
-  array (
+return array(
+  0 => array(
     'name' => 'PricesetIndividualContribution',
     'class' => 'CRM_Pricesetfrequency_DAO_PricesetIndividualContribution',
     'table' => 'civicrm_priceset_individual_contribution',
