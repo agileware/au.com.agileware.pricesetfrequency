0b91e3d27c423aeaf7bdc985d3c0f981e85c7a00
agileware-fj <agileware-fj@users.noreply.github.com>
2025-03-26 18:42:45 +0000

    CIVIBLD-287 [AUTO] civix upgraded
    Files changed:
    M	CRM/Pricesetfrequency/DAO/PricesetIndividualContribution.php
    M	CRM/Pricesetfrequency/Upgrader.php
    D	CRM/Pricesetfrequency/Upgrader/Base.php
    M	info.xml
    A	mixin/entity-types-php@2.0.0.mixin.php
    A	mixin/lib/civimix-schema@5.80.2/pathload.main.php
    A	mixin/lib/civimix-schema@5.80.2/src/AutomaticUpgrader.php
    A	mixin/lib/civimix-schema@5.80.2/src/CiviMixSchema.php
    A	mixin/lib/civimix-schema@5.80.2/src/DAO.php
    A	mixin/lib/civimix-schema@5.80.2/src/SchemaHelper.php
    A	mixin/lib/civimix-schema@5.80.2/src/SchemaHelperInterface.php
    A	mixin/lib/civimix-schema@5.80.2/src/SqlGenerator.php
    A	mixin/lib/pathload-0.php
    A	mixin/smarty-v2@1.0.3.mixin.php
    M	pricesetfrequency.civix.php
    M	pricesetfrequency.php
    A	schema/PricesetIndividualContribution.entityType.php
    D	sql/auto_install.sql
    D	sql/auto_uninstall.sql
    D	xml/schema/CRM/Pricesetfrequency/PricesetIndividualContribution.entityType.php
    D	xml/schema/CRM/Pricesetfrequency/PricesetIndividualContribution.xml

--- pricesetfrequency.php
+++ pricesetfrequency.php
@@ -13,15 +13,6 @@ function pricesetfrequency_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function pricesetfrequency_civicrm_xmlMenu(&$files) {
-  _pricesetfrequency_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -31,107 +22,12 @@ function pricesetfrequency_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function pricesetfrequency_civicrm_postInstall() {
-  _pricesetfrequency_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function pricesetfrequency_civicrm_uninstall() {
-  _pricesetfrequency_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function pricesetfrequency_civicrm_enable() {
   _pricesetfrequency_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function pricesetfrequency_civicrm_disable() {
-  _pricesetfrequency_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function pricesetfrequency_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _pricesetfrequency_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function pricesetfrequency_civicrm_managed(&$entities) {
-  _pricesetfrequency_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function pricesetfrequency_civicrm_caseTypes(&$caseTypes) {
-  _pricesetfrequency_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function pricesetfrequency_civicrm_angularModules(&$angularModules) {
-  _pricesetfrequency_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function pricesetfrequency_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _pricesetfrequency_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
- */
-function pricesetfrequency_civicrm_entityTypes(&$entityTypes) {
-  _pricesetfrequency_civix_civicrm_entityTypes($entityTypes);
 }
 
 /**
